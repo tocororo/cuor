81bdd0090b6f27cec4036d0589664d1314ed2b37
Rafael Martinez Estevez <malayibiri@gmail.com>
2021-09-19 10:31:28 -0400

    wikidata.log

--- cuor/harvester/wikidata/logger_base.py
+++ cuor/harvester/wikidata/logger_base.py
@@ -1,12 +1,12 @@
 import logging
-
+from cuor.configvariables import WIKIDATA_LOG
 logger = logging
 
 logger.basicConfig(level=logging.DEBUG,
                    format='%(asctime)s: %(levelname)s [%(filename)s:%(lineno)s] %(message)s',
                    datefmt='%I:%M:%S %p',
                    handlers=[
-                       logging.FileHandler('/var/log/wikidata.log'),
+                       logging.FileHandler(WIKIDATA_LOG),
                        logging.StreamHandler()
                    ])
 
